42f59da15af026d3a5c65e2b9ef4f32d5856ae5e
sohanemon <m.sohanemon@gmail.com>
2023-08-12 10:18:39 +0600

    feat: zustand sample store

--- app/page.tsx
+++ app/page.tsx
@@ -1,59 +1,19 @@
+"use client"
+
+import { useEffect } from "react"
+import useUser from "@/store/user"
+
 export default function IndexPage() {
+  const [fetchUser, users] = useUser((s) => [s.fetchUser, s.users])
+  useEffect(() => {
+    fetchUser()
+  }, [fetchUser])
+  console.log(users)
   return (
     <section className="container grid items-center">
-      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Beatae ut
-      cupiditate perferendis et accusamus ipsa. Non delectus autem tempora ut,
-      vero ex, ipsum eaque corrupti consequatur aspernatur eligendi! Aliquam
-      quos, vel voluptatibus corporis odio suscipit reiciendis et repudiandae
-      possimus quasi mollitia ipsam! Amet sequi ab dolore obcaecati? Facilis
-      tempora dolorem est sed consectetur aliquam velit reprehenderit iusto?
-      Facere, voluptatum totam. Est tenetur at voluptate dolorem, aliquam
-      voluptatem quas atque nihil iure reprehenderit quia vel, eligendi nesciunt
-      porro illum ipsum, quos quis voluptatibus similique sed a totam enim rem
-      dignissimos. Qui ducimus perferendis cumque facilis, quam officia odio,
-      animi repellat culpa debitis, laudantium velit vero aliquam alias unde
-      laborum eius sapiente nemo sequi ipsa nesciunt voluptatem architecto ut?
-      Cum fuga ratione, eveniet dolor neque sed! Sit earum dolores nihil sunt,
-      aut suscipit iusto. Magnam ad architecto quis beatae porro ab eum saepe
-      unde a laudantium, numquam doloribus ratione explicabo repellat natus
-      similique dignissimos odit eaque rerum autem modi soluta distinctio. Eos
-      sit aspernatur dicta veritatis esse sunt architecto repellat, saepe animi
-      cupiditate fugiat aut, qui nisi. Fuga similique vitae quidem dolorum
-      mollitia nesciunt soluta suscipit provident laudantium laborum dicta
-      debitis perferendis, error repudiandae ab architecto dolore eligendi. Non
-      laborum doloribus reiciendis minima nulla repellat, tenetur corporis
-      voluptatum impedit eaque odio architecto doloremque vel dolore pariatur.
-      Blanditiis ducimus commodi, at earum delectus enim ratione doloremque
-      voluptas, fugit illo assumenda! Ab minus inventore non, voluptas eveniet
-      quia molestiae aliquid excepturi qui, iste repellendus ex dolorem
-      consequatur laudantium modi officia quisquam? Error atque odio incidunt
-      similique consequuntur adipisci ad deleniti obcaecati expedita assumenda
-      fugit soluta quasi blanditiis labore est, quaerat rem rerum unde molestiae
-      repellat eos! Doloribus asperiores excepturi sapiente soluta nulla
-      explicabo, velit, aliquam in distinctio possimus deleniti dolorum, aperiam
-      eum. Quisquam, omnis? Similique ad, provident nobis ratione molestiae
-      soluta saepe quibusdam placeat facere commodi iusto cupiditate sunt, dolor
-      aliquam quia labore minima praesentium laboriosam quasi, dignissimos eos
-      molestias delectus culpa vitae. Voluptatum dicta facere quis, eveniet enim
-      doloribus explicabo est officia quidem sunt cupiditate aut labore corrupti
-      libero sit laborum veritatis! Consequuntur culpa officiis minus deleniti
-      ipsum, ducimus debitis veniam eius, explicabo nobis veritatis
-      reprehenderit omnis, nemo quod distinctio. Ut adipisci laborum quos soluta
-      totam nostrum exercitationem molestias, voluptates odit ex ipsa ea fuga
-      ipsum! Ex enim minus tenetur, laudantium velit tempore soluta accusantium
-      quasi ullam aut, dignissimos corrupti voluptatibus blanditiis iure?
-      Sapiente voluptatum asperiores ipsum veritatis, esse ipsam beatae
-      quibusdam doloribus suscipit impedit repellat non laborum laboriosam,
-      explicabo tempora facere quos. Nihil quos neque, esse veniam excepturi
-      labore quam rerum, ipsam ipsa laborum atque veritatis provident possimus
-      molestias minus sunt illum aliquam quidem. Ipsam ad harum sed aliquid
-      error dolorem, distinctio itaque vero voluptatibus voluptatem
-      exercitationem quam repellat magni. Soluta nihil, ipsa consequuntur ab
-      nobis illo esse vero accusamus assumenda quod fuga, repellat impedit, iure
-      qui corporis quidem enim reiciendis in maiores? Mollitia reiciendis, eum
-      praesentium omnis porro recusandae earum voluptate esse nemo sequi cum
-      quas consequatur aperiam asperiores, sed magni officiis ea nesciunt ut
-      doloremque quibusdam quos? Corporis, ea beatae.
+      {users?.map((_) => (
+        <p key={_.name}>{_.name}</p>
+      ))}
     </section>
   )
 }
