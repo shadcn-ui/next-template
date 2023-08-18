d5dbb5be8eb2ceda74715c1c95e2fd1a9b770565
sohanemon <m.sohanemon@gmail.com>
2023-08-18 18:21:06 +0600

    added figma by default

---
+++ next.config.ts
@@ -0,0 +1,15 @@
+import { NextConfig } from 'next';
+
+const nextConfig: NextConfig = {
+  reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'www.figma.com',
+      },
+    ],
+  },
+};
+
+export default nextConfig;
