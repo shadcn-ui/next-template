42b4a8f70d1ba459ee157e3356d8df178090f6f8
sohanemon <m.sohanemon@gmail.com>
2023-08-22 11:48:40 +0600

    chore: blur images

--- components/images.tsx
+++ components/images.tsx
@@ -1,7 +1,12 @@
 import Image from 'next/image';
 
+import Img from './img';
+
 export const Images = {
   vercel: (props: any) => (
     <Image {...props} src={`/vercel.svg`} alt="    " width={600} height={300} />
+  ),
+  cat1: (props: any) => (
+    <Img {...props} src="/public/assets/images/Clipboard-1.png" />
   ),
 };
