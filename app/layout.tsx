a317a212ec7690448ca660cd42db721870a20bea
sohanemon <m.sohanemon@gmail.com>
2023-08-27 15:49:16 +0600

    feat: next/dynamic

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,13 +4,15 @@ import TransitionProvider from '@/context/transition-provider';
 import '@/styles/custom.css';
 import '@/styles/globals.css';
 import { Metadata } from 'next';
+import dynamic from 'next/dynamic';
 
 import { siteConfig } from '@/config/site';
 import { fontSans } from '@/lib/fonts';
 import { cn } from '@/lib/utils';
-import GoToTop from '@/components/goto-top';
-import Navbar from '@/components/navbar';
 import { TailwindIndicator } from '@/components/tailwind-indicator';
+
+const GoToTop = dynamic(() => import('@/components/goto-top'));
+const Navbar = dynamic(() => import('@/components/navbar'));
 
 export const metadata: Metadata = {
   title: {
