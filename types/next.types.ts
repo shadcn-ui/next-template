37425dbb44b2a04b5d82ac294ffbc5568e8012ef
sohanemon <m.sohanemon@gmail.com>
2023-09-13 13:56:51 +0600

    feat: element event type

--- types/next.types.ts
+++ types/next.types.ts
@@ -2,3 +2,7 @@ export type PageProps = Readonly<{
   params: { slug: string };
   searchParams: { [key: string]: string | string[] | undefined };
 }>;
+
+export interface ElementEvent<T> {
+  target: T;
+}
