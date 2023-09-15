5384da72a99829a309f0e6c16f8a45b52bde3f85
sohanemon <m.sohanemon@gmail.com>
2023-09-15 06:10:31 +0600

    typed route

--- next.config.mjs
+++ next.config.mjs
@@ -1,25 +1,19 @@
 // @ts-check
-import withPlaiceholder from '@plaiceholder/next';
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  experimental: {
+    typedRoutes: true,
+  },
   images: {
     remotePatterns: [
       {
         protocol: 'https',
         hostname: 'www.figma.com',
       },
-      {
-        protocol: 'https',
-        hostname: 'images.pexels.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-      },
     ],
   },
 };
 
-export default withPlaiceholder(nextConfig);
+export default nextConfig;
