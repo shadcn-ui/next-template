bb0a966e6962e82d9f01ec64dcdaae443786c30b
sohanemon <m.sohanemon@gmail.com>
2023-09-16 19:57:39 +0600

    center util

--- styles/plugins/utilities.ts
+++ styles/plugins/utilities.ts
@@ -4,5 +4,7 @@ export const utilities = plugin(({ addUtilities }) => {
   addUtilities({
     '.scrollbar-hidden::-webkit-scrollbar': { display: 'none' },
     '.scrollbar-hidden': { msOverflowStyle: 'none', scrollbarWidth: 'none' },
+    '.center-x': { '@apply ml-[50%] mt-4 -translate-x-1/2': {} },
+    '.center-y': { '@apply mt-[50%] mt-4 -translate-y-1/2': {} },
   });
 });
