3f8b74d99d5d78b5dc90bc21c0d1f32a35de770f
sohanemon <m.sohanemon@gmail.com>
2023-10-08 13:12:20 +0600

    Added variants: `shrinked` and `collapsed`

--- config/variants/motion.variant.ts
+++ config/variants/motion.variant.ts
@@ -5,16 +5,8 @@ export const defaultVariants: Variants = {
   right: { x: 300, opacity: 0 },
   top: { y: -300, opacity: 0 },
   bottom: { y: 100, opacity: 0 },
+  shrinked: { scaleX: 0, transformOrigin: 'left' },
+  collapsed: { scaleY: 0, transformOrigin: 'top' },
   hidden: { opacity: 0 },
-  visible: { x: 0, y: 0, opacity: 1, scale: 1 },
-  stagger: {
-    x: 0,
-    y: 0,
-    opacity: 1,
-    scale: 1,
-    transition: {
-      delayChildren: 0,
-      staggerChildren: 0.3,
-    },
-  },
+  visible: { x: 0, y: 0, opacity: 1, scale: 1, scaleX: 1, scaleY: 1 },
 };
