e6dedd4ca8a981ffa19fcd7e801e7d3d74599dc0
sohanemon <m.sohanemon@gmail.com>
2023-12-11 11:40:32 +0600

    chore: Remove production deployment workflow
    The production deployment workflow has been removed.

---
+++ src/lib/helper.ts
@@ -0,0 +1,28 @@
+export function debounce<T extends (...args: any[]) => void>(
+  func: T,
+  wait: number = 1000
+): (...args: Parameters<T>) => void {
+  let timeout: ReturnType<typeof setTimeout> | undefined;
+  return function (this: ThisParameterType<T>, ...args: Parameters<T>): void {
+    const context = this;
+    clearTimeout(timeout);
+    timeout = setTimeout(() => func.apply(context, args), wait);
+  };
+}
+
+export function throttle<T extends (...args: any[]) => void>(
+  func: T,
+  wait: number = 1000
+): (...args: Parameters<T>) => void {
+  let inThrottle: boolean = false;
+  return function (this: ThisParameterType<T>, ...args: Parameters<T>): void {
+    const context = this;
+    if (!inThrottle) {
+      func.apply(context, args);
+      inThrottle = true;
+      setTimeout(() => {
+        inThrottle = false;
+      }, wait);
+    }
+  };
+}
