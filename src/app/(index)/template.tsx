cac5b05e93574fe50069c76579047de0a5fd3acb
sohanemon <m.sohanemon@gmail.com>
2024-01-13 21:33:31 +0600

    refactor(context): Remove transition provider

---
+++ src/app/(index)/template.tsx
@@ -0,0 +1,15 @@
+import { Motion } from '@/components/motion';
+
+interface RootTemplateProps {
+  children: React.ReactNode;
+}
+
+export default function RootTemplate({ children }: RootTemplateProps) {
+  return (
+    <>
+      <Motion initial="bottom" transition={{ duration: 0.4 }}>
+        {children}
+      </Motion>
+    </>
+  );
+}
--- src/context/providers.tsx
+++ src/context/providers.tsx
@@ -6,12 +6,10 @@ import { ThemeProvider } from 'next-themes';
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <>
-      {/* <TransitionProvider as={'main'} className="flex-1" initial="top"> */}
       <ThemeProvider defaultTheme="system">
         {children}
         <TailwindIndicator />
       </ThemeProvider>
-      {/* </TransitionProvider> */}
     </>
   );
 }
