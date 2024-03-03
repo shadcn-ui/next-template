008cd0db5fe4c59ff753c45601812012c7909d0d
sohanemon <m.sohanemon@gmail.com>
2024-03-03 19:54:48 +0600

    feat(layout.tsx): added 'default' class to html tag refactor(colors.ts): updated color variables refactor(motion.variant.ts): modified animation variants

--- config/variants/motion.variant.ts
+++ config/variants/motion.variant.ts
@@ -5,8 +5,9 @@ const defaultVariants = {
   right: { x: 300, opacity: 0 },
   top: { y: -300, opacity: 0 },
   bottom: { y: 100, opacity: 0 },
-  shrinked: { scaleX: 0, transformOrigin: 'left' },
-  collapsed: { scaleY: 0, transformOrigin: 'top' },
+  collapsed: { opacity: 0, scale: 0 },
+  'collapsed-x': { scaleX: 0, transformOrigin: 'left' },
+  'collapsed-y': { scaleY: 0, transformOrigin: 'top' },
   hidden: { opacity: 0 },
   visible: { x: 0, y: 0, opacity: 1, scale: 1, scaleX: 1, scaleY: 1 },
 } satisfies Variants;
